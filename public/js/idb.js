ac9a88f9293f0f7b9cd52391ab5f98443e4d61d6
Josiah-Canada <josiahcanada@yahoo.com>
2022-08-14 23:34:25 -0400

    trouble

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,14 +1,5 @@
 let db;
 
-// db.enablePersistence()
-// .catch(err => {
-//   if(err.code == 'failed-precondition'){
-//     console.log("persistence failed")
-//   } else if(err.code == 'unimplemented') {
-//     console.log("persistence is not available")
-//   }
-// })
-
 const request = indexedDB.open("Budget_tracker", 1);
 
 request.onupgradeneeded = function (event) {
--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budgets";
 
-mongoose.connect("mongodb://localhost/budget", process.env.MONGODB_URI, {
+mongoose.connect("mongodb://localhost/budgets", process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
